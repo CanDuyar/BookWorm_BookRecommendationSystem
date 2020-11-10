4de11bbc1add6a5d7c58355a05354657968b3775
ashrafyawar <mayawar@gtu.edu.tr>
2020-11-10 03:53:34 +0300

    changes

--- src/math.py
+++
@@ -1,6 +0,0 @@
-class Math():
-    def addition(value1, value2):
-        if not isinstance(value1, int) and not isinstance(value2, int):
-            return 'Invalid input'
-        else:
-            return value1 + value2
--- test/math-test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from src.math import Math
-
-
-class MathTest(unittest.TestCase):
-    def test_addition(self):
-        # Make test fail
-        self.assertEqual(Math.addition(3, 4), 8)
