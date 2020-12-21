6b0a6a1d9d9d1cfad6f589655db2740d1baf4f57
ashrafyawar <mayawar@gtu.edu.tr>
2020-12-21 19:05:00 +0300

    deplo

--- Book_Worm_WebApp/settings.py
+++ Book_Worm_WebApp/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Book_worm_WebApp/static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Book_worm_WebApp/static')]
 
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
