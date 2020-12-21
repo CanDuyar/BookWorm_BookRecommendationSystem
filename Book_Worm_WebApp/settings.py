a2ff5569a9ec7e875bb97866f5fe2a3752a0d851
ashrafyawar <mayawar@gtu.edu.tr>
2020-12-21 19:43:59 +0300

    redeploy

--- Book_Worm_WebApp/settings.py
+++ Book_Worm_WebApp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import sys
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,3 +132,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 IMPORT_EXPORT_USE_TRANSACTIONS = True
+
+django_heroku.settings(locals())
