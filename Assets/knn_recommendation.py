a5140502f5a27cfff177a4700946bd458e484eb8
Canberk Arıcı <45467111+canberkarc@users.noreply.github.com>
2021-01-27 00:46:43 +0200

    Add files via upload
    model kitap turune gore sonuc verecek hale getirildi

--- Assets/knn_recommendation.py
+++ Assets/knn_recommendation.py
@@ -5,7 +5,7 @@ from sklearn.neighbors import NearestNeighbors
 import joblib
 from sklearn.preprocessing import LabelEncoder
 
-df = pd.read_csv("/home/canberk/Desktop/Bookworm/data.csv")
+df = pd.read_csv("/home/canberk/Desktop/lEncodeddata.csv")
 
 '''
 # Dataframedeki virgulleri silme kodu
@@ -16,13 +16,12 @@ for i in df.columns:
 df.to_csv("data.csv")
 '''
 
-#print(df.Genres)
-
 '''
 labelenc = LabelEncoder()
-df.Genres = labelenc.fit_transform(df.Genres)
-df.to_csv("data.csv")
+df.genres = labelenc.fit_transform(df.genres)
+df.to_csv("lEncodeddata.csv")
 '''
+
 
 df_pivot = df.pivot(index = "book_id", columns = "genres", values = "rating").fillna(0)
 
@@ -31,12 +30,19 @@ matrix = csr_matrix(df_pivot.values)
 
 knn = joblib.load('knn.h5')
 
-#knn = NearestNeighbors(metric = 'cosine', algorithm = 'brute')
 knn.fit(matrix)
-#joblib.dump(knn, 'knn.h5')
 
 
-query_index = np.random.choice(df_pivot.shape[0])
+genre = a  # BURAYA KULLANICININ GIRDIGI KITAP TURU (GENRE) GELECEK (INTEGER OLARAK)
+query_index = 0
+filter1 = df.genres == genre
+ids = df.book_id.where(filter1)
+for i in ids:
+	if type(i) == float and pd.isna(i):
+		pass
+	else:
+		query_index = int(i)
+
 distances, indices = knn.kneighbors(df_pivot.iloc[query_index, :].values.reshape(1, -1), n_neighbors = 6)
 
 for i in range(0, len(distances.flatten())):
