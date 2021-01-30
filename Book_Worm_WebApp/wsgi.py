cfe38ec0a526d0cf27097829109f236765b88131
ashrafyawar <mayawar@gtu.edu.tr>
2021-01-31 02:32:15 +0300

    final demo preps

--- Book_Worm_WebApp/wsgi.py
+++ Book_Worm_WebApp/wsgi.py
@@ -10,9 +10,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Book_Worm_WebApp.settings')
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", __file__)
-import django
-
-django.setup()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Book_Worm_WebApp.settings')
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", __file__)
+# import django
+#
+# django.setup()
 application = get_wsgi_application()
